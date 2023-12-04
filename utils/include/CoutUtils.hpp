22a0afe00ed735bfd4bd45b8f1fe62e373744acb
Justin Brouwer <justin.brouwer@heattransformers.com>
2023-12-04 18:37:28 +0100

    Day4-A&B

--- utils/include/CoutUtils.hpp
+++ utils/include/CoutUtils.hpp
@@ -6,6 +6,18 @@
 #include <functional>
 
 namespace CoutUtils {
+    std::string convertToString(const std::string & name, const std::vector<std::string> & input) {
+        std::string ret = name + ": [";
+        for (unsigned int i=0; i < input.size(); i++) {
+            ret += input[i];
+            if (i+1 < input.size()) {
+                ret += ", ";
+            }
+        }
+        ret += "]";
+        return ret;
+    }
+
     template<typename T>
     std::string convertToString(const std::string & name, const std::vector<T> & input) {
         std::string ret = name + ": [";
--- utils/include/VectorUtils.hpp
+++ utils/include/VectorUtils.hpp
@@ -4,10 +4,39 @@
 
 namespace VectorUtils {
 
-template<typename T>
+    template<typename T>
     std::vector<T> concatenate(const std::vector<T> & lhs, const std::vector<T> & rhs) {
         std::vector<T> ret = lhs;
         ret.insert(ret.end(), rhs.begin(), rhs.end());
+        return ret;
+    }
+
+    template<typename T>
+    bool contains(const std::vector<T> & lhs, const T & rhs) {
+        return std::find(lhs.begin(), lhs.end(), rhs) != lhs.end();
+    }
+
+    template<typename T>
+    std::vector<T> remove(const std::vector<T> & lhs, const std::vector<T> & rhs) {
+        std::vector<T> ret;
+        for (const auto & t : lhs) {
+            if (!contains(rhs, t)) {
+                ret.push_back(t);
+            }
+        }
+        return ret;
+    }
+
+    template<typename T>
+    std::vector<T> intersection(std::vector<T> lhs, std::vector<T> rhs) {
+        std::vector<T> ret;
+
+        std::sort(lhs.begin(), lhs.end());
+        std::sort(rhs.begin(), rhs.end());
+
+        std::set_intersection(lhs.begin(),lhs.end(),
+                              rhs.begin(),rhs.end(),
+                              back_inserter(ret));
         return ret;
     }
 };
