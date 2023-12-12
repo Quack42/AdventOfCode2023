b8fded4e9669d43d197ad8b7452d4e8872c8fc22
Justin Brouwer <justin.brouwer@heattransformers.com>
2023-12-12 21:09:40 +0100

    Added template directory, to save time creating a new day.

---
+++ template/main.cpp
@@ -0,0 +1,97 @@
+#include "CoordinateUtils.hpp"
+#include "CoutUtils.hpp"
+#include "NumberUtils.hpp"
+#include "StringUtils.hpp"
+#include "VectorUtils.hpp"
+#include "RangeUtils.hpp"
+
+#include <cassert>
+#include <cctype>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+
+
+using puzzleValueType = long long int;
+
+const std::string givenTestData_problem1 = "";
+
+constexpr puzzleValueType expectedSolution_problem1 = -1;
+
+template<typename T>
+puzzleValueType solve1(T & stream) {
+    // convert to lines
+    std::vector<std::string> lines;
+    for (std::string line; std::getline(stream, line);) {
+        lines.push_back(line);
+    }
+
+    puzzleValueType puzzleValue = 0;
+
+    return puzzleValue;
+}
+
+const std::string givenTestData_problem2 = "";
+
+constexpr puzzleValueType expectedSolution_problem2 = -1;
+
+template<typename T>
+puzzleValueType solve2(T & stream) {
+    // convert to lines
+    std::vector<std::string> lines;
+    for (std::string line; std::getline(stream, line);) {
+        lines.push_back(line);
+    }
+
+    puzzleValueType puzzleValue = 0;
+
+    return puzzleValue;
+}
+
+int main(int argc, char ** argv) {
+    std::stringstream inputStream;
+    if (argc == 1) {
+        std::stringstream strStream_problem1(givenTestData_problem1);
+        const auto solve1_solution = solve1(strStream_problem1);
+        std::stringstream strStream_problem2(givenTestData_problem2);
+        const auto solve2_solution = solve2(strStream_problem2);
+
+        if (solve1_solution == expectedSolution_problem1) {
+            std::cout << "Puzzle A <Success!> :[" << solve1_solution << "]" << std::endl;
+        } else {
+            std::cout << "Puzzle A <MISMATCH> :[" << solve1_solution << "]" << std::endl;
+        }
+
+        if (solve2_solution == expectedSolution_problem2) {
+            std::cout << "Puzzle B <Success!> :[" << solve2_solution << "]" << std::endl;
+        } else {
+            std::cout << "Puzzle B <MISMATCH> :[" << solve2_solution << "]" << std::endl;
+        }
+    } else if (argc == 2 || argc == 3) {
+        std::string url(argv[1]);
+        std::cout << "Trying to input file:[" << url << "]" << std::endl;
+        std::filesystem::path path(url);
+
+        std::ifstream fileStream;
+        fileStream.open(path);
+        if (!fileStream.is_open()) {
+            std::cout << "fileStream is NOT Open!" << std::endl;
+            return -1;
+        }
+
+        if (argc == 2 || (argc == 3 && argv[2][0] == 'A')) {
+            auto solve1_solution = solve1(fileStream);
+            std::cout << "Puzzle A result :[" << solve1_solution << "]" << std::endl;
+        } else if(argc == 3 && argv[2][0] == 'B') {
+            auto solve2_solution = solve2(fileStream);
+            std::cout << "Puzzle B result :[" << solve2_solution << "]" << std::endl;
+        }
+    }
+
+    return 0;
+}
